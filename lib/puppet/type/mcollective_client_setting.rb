e856cc022ac3a5eed6de35570e10c1f2764efe13
Richard Clamp <richardc@unixbeard.net>
2013-06-10 18:22:35 +0100

    inifile's documentation is misleading
    You have to implement the key_val_separator parameter in your parent class else
    boom

--- lib/puppet/type/mcollective_client_setting.rb
+++ lib/puppet/type/mcollective_client_setting.rb
@@ -8,4 +8,17 @@ Puppet::Type.newtype(:mcollective_client_setting) do
   newproperty(:value) do
     desc 'The value of the setting to be defined.'
   end
+
+  newparam(:key_val_separator) do
+    desc 'The separator string to use between each setting name and value. ' +
+         'Defaults to " = ", but you could use this to override e.g. whether ' +
+         'or not the separator should include whitespace.'
+    defaultto(" = ")
+
+    validate do |value|
+      unless value.scan('=').size == 1
+        raise Puppet::Error, ":key_val_separator must contain exactly one = character."
+      end
+    end
+  end
 end
