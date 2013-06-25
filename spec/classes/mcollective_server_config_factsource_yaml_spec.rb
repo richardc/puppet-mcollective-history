46591e92346650f9231dac41da67b74950bcf48a
Richard Clamp <richardc@unixbeard.net>
2013-06-25 17:10:31 +0100

    ensure ordering of facts.yaml

--- spec/classes/mcollective_server_config_factsource_yaml_spec.rb
+++ spec/classes/mcollective_server_config_factsource_yaml_spec.rb
@@ -4,6 +4,9 @@ describe 'mcollective::server::config::factsource::yaml' do
   it { should contain_class('mcollective::server::config::factsource::yaml') }
   it { should contain_mcollective_server_setting('factsource').with_value('yaml') }
   it { should contain_file('/etc/mcollective/facts.yaml') }
-  it { should contain_file('/etc/mcollective/facts.yaml').with_content(/osfamily: RedHat/) }
-  it { should contain_file('/etc/mcollective/facts.yaml').with_content(/number_of_cores: 42/) }
+  it { should contain_file('/etc/mcollective/facts.yaml').with_content(/^  osfamily: RedHat/) }
+  it { should contain_file('/etc/mcollective/facts.yaml').with_content(/^  number_of_cores: 42/) }
+  it "should be alpha-sorted" do
+    should contain_file('/etc/mcollective/facts.yaml').with_content(/^  number_of_cores:.*?^  osfamily:/m)
+  end
 end
