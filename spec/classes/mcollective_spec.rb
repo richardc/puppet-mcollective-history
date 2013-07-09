1e927c98f78a7eabeb72d6db14f9c2f11f653136
Richard Clamp <richardc@unixbeard.net>
2013-07-09 13:44:55 +0100

    rename the example groups to be terser

--- spec/classes/mcollective_spec.rb
+++ spec/classes/mcollective_spec.rb
@@ -8,12 +8,12 @@ describe 'mcollective' do
     let(:params) { { :server => true } }
     it { should contain_class('mcollective::server') }
 
-    describe "factsource" do
-      it "should default to a yaml factsource" do
+    describe "#factsource" do
+      it "should default to yaml" do
         should contain_mcollective__server__setting('factsource').with_value('yaml')
       end
 
-      describe "yaml factsource" do
+      describe "yaml" do
         let(:facts) { { :osfamily => 'RedHat', :number_of_cores => 42 } }
         it { should contain_file('/etc/mcollective/facts.yaml') }
         it { should contain_file('/etc/mcollective/facts.yaml').with_content(/^  osfamily: RedHat/) }
@@ -23,19 +23,19 @@ describe 'mcollective' do
         end
       end
 
-      describe "facter factsource" do
+      describe "facter" do
         let(:params) { { :server => true, :factsource => 'facter' } }
         it { should contain_mcollective__server__setting('factsource').with_value('facter') }
         it { should contain_package('mcollective-facter-facts') }
       end
     end
 
-    describe "connector" do
-      it "should default to the activemq connector" do
+    describe "#connector" do
+      it "should default to activemq" do
         should contain_mcollective__server__setting('connector').with_value('activemq')
       end
 
-      describe "activemq connector" do
+      describe "activemq" do
         describe "setting connectors" do
           let(:params) { { :server => true, :activemq_hosts => %w{ foo bar } } }
           it { should contain_mcollective__server__setting('plugin.activemq.pool.size').with_value(2) }
