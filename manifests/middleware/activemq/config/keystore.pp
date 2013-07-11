a78cd71049ab5ec2d8097806f13bff595384a4ad
Richard Clamp <richardc@unixbeard.net>
2013-07-11 22:05:24 +0100

    copy certs and private_key to the activemq
    If you put the middleware on a node without an mcollective::server you can't
    copy the certs from there, so duplicate them.

--- manifests/middleware/activemq/config/keystore.pp
+++ manifests/middleware/activemq/config/keystore.pp
@@ -1,31 +1,38 @@
 # private class
 class mcollective::middleware::activemq::config::keystore {
+  file { "${mcollective::activemq_confdir}/ca.pem":
+    source => $mcollective::ssl_ca_cert,
+  } ->
   java_ks { 'mcollective:truststore':
     ensure       => 'latest',
-    certificate  => '/etc/mcollective/ca.pem',
+    certificate  => "${mcollective::activemq_confdir}/ca.pem",
     target       => "${mcollective::activemq_confdir}/truststore.jks",
     password     => 'puppet',
     trustcacerts => true,
   } ->
-
   file { "${mcollective::activemq_confdir}/truststore.jks":
     owner => 'activemq',
     group => 'activemq',
-    mode  => '0600',
+    mode  => '0400',
   }
 
+  file { "${mcollective::activemq_confdir}/server_public.pem":
+    source => $mcollective::ssl_server_public,
+  } ->
+  file { "${mcollective::activemq_confdir}/server_private.pem":
+    source => $mcollective::ssl_server_private,
+  } ->
   java_ks { 'mcollective:keystore':
     ensure       => 'latest',
-    certificate  => '/etc/mcollective/server_public.pem',
-    private_key  => '/etc/mcollective/server_private.pem',
+    certificate  => "${mcollective::activemq_confdir}/server_public.pem",
+    private_key  => "${mcollective::activemq_confdir}/server_private.pem",
     target       => "${mcollective::activemq_confdir}/keystore.jks",
     password     => 'puppet',
     trustcacerts => true,
   } ->
-
   file { "${mcollective::activemq_confdir}/keystore.jks":
     owner => 'activemq',
     group => 'activemq',
-    mode  => '0600',
+    mode  => '0400',
   }
 }
