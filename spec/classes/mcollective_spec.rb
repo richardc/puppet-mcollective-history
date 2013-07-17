527146f6a951fa55f97bdf402a541d11dcb08dca
Richard Clamp <richardc@unixbeard.net>
2013-07-17 17:17:14 +0100

    Test the activemq middleware
    This also indirectly test that we're using the puppetlabs activemq and
    java_ks modules rather than our own stuff.

--- spec/classes/mcollective_spec.rb
+++ spec/classes/mcollective_spec.rb
@@ -3,6 +3,7 @@ describe 'mcollective' do
   it { should contain_class('mcollective') }
   it { should_not contain_class('mcollective::server') }
   it { should_not contain_class('mcollective::client') }
+  it { should_not contain_class('mcollective::middleware') }
 
   describe "installing a server" do
     let(:params) { { :server => true } }
@@ -61,6 +62,38 @@ describe 'mcollective' do
         let(:params) { { :server => true, :securityprovider => 'psk' } }
         it { should contain_mcollective__server__setting('securityprovider').with_value('psk') }
         it { should contain_mcollective__server__setting('plugin.psk').with_value('changeme') }
+      end
+    end
+  end
+
+  describe "installing middleware" do
+    let(:params) { { :middleware => true } }
+    it { should contain_class('mcollective::middleware') }
+
+    context "#connector" do
+      it "should default to apache" do
+        should contain_class('mcollective::middleware::activemq')
+      end
+
+      context "activemq" do
+        it { should contain_class('activemq') }
+        it { should contain_class('activemq').with_instance('mcollective') }
+
+        context "#middleware_ssl" do
+          it "should default to true" do
+            # We create a truststore when we're doing tls
+            should contain_java_ks('mcollective:truststore')
+          end
+
+          context "true" do
+            it { should contain_java_ks('mcollective:truststore').with_password('puppet') }
+          end
+
+          context "false" do
+            let(:params) { { :middleware => true, :middleware_ssl => false } }
+            it { should_not contain_java_ks('mcollective:truststore') }
+          end
+        end
       end
     end
   end
