0b25d680f5f02037a6d14f6392fb4f85ea676d02
Richard Clamp <richardc@unixbeard.net>
2013-07-24 16:29:42 +0100

    redis connector supprts direct_addressing

--- manifests/common/config/connector/redis.pp
+++ manifests/common/config/connector/redis.pp
@@ -1,5 +1,13 @@
 # Private class
 class mcollective::common::config::connector::redis {
+  mcollective::common::setting { 'direct_addressing':
+    value => 'yes',
+  }
+
+  mcollective::common::setting { 'direct_addressing_threshold':
+    value => 5,
+  }
+
   # Redis connector only uses one host to connect to.  Assume that the first will
   # be OK
   mcollective::common::setting { 'plugin.redis.host':
