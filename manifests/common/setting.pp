6ed85354c840c1c2c6531e3aa81148d23f9fe9ad
Richard Clamp <richardc@unixbeard.net>
2013-08-02 11:23:39 +0100

    test mcollective::common::setting
    Alter the setting name to match the other mcollective::server::setting and
    mcollective::user::setting defines.

--- manifests/common/setting.pp
+++ manifests/common/setting.pp
@@ -1,6 +1,6 @@
 # Define - mcollective::common::setting
 define mcollective::common::setting($value) {
-  mcollective::setting { "mcollective::common ${name}":
+  mcollective::setting { "mcollective::common::setting ${name}":
     setting => $name,
     value   => $value,
     target  => [ 'mcollective::server', 'mcollective::client' ],
