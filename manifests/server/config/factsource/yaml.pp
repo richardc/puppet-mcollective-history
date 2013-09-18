94feb46e81ee04ef77b00d82f596dfcc5f300e5d
Richard Clamp <richardc@unixbeard.net>
2013-09-18 14:46:08 +0100

    note where the YAML recipe came from

--- manifests/server/config/factsource/yaml.pp
+++ manifests/server/config/factsource/yaml.pp
@@ -3,12 +3,9 @@ class mcollective::server::config::factsource::yaml {
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
-  # The facts.yaml file resource is generated in its own dedicated class
-  # By doing this, the file produced isn't polluted with unwanted in scope
-  # class variables.
-  # Also we're not in the config class as we don't need to restart the service
-  # when the facts just change.
 
+  # This pattern originally from
+  # http://projects.puppetlabs.com/projects/mcollective-plugins/wiki/FactsFacterYAML
   file { $mcollective::yaml_fact_path_real:
     owner   => root,
     group   => root,
