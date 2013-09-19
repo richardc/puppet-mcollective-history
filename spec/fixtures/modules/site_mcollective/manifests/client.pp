cf6bb2b832a1ad0a618d0e507accc56a8630c650
Richard Clamp <richardc@unixbeard.net>
2013-09-19 11:36:31 +0100

    for convenience, add client config to the vagrant user

--- spec/fixtures/modules/site_mcollective/manifests/client.pp
+++ spec/fixtures/modules/site_mcollective/manifests/client.pp
@@ -5,4 +5,9 @@ class site_mcollective::client {
     certificate => "puppet:///modules/${module_name}/certs/root.pem",
     private_key => "puppet:///modules/${module_name}/private_keys/root.pem",
   }
+
+  mcollective::user { 'vagrant':
+    certificate => "puppet:///modules/${module_name}/certs/vagrant.pem",
+    private_key => "puppet:///modules/${module_name}/private_keys/vagrant.pem",
+  }
 }
