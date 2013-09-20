ffbb14ad57cfa55c3fc0e69563bc3a84d9957458
Richard Clamp <richardc@unixbeard.net>
2013-09-20 21:07:02 +0100

    these defaults changed, so don't talk about them being defaults, just test

--- spec/defines/mcollective__user_spec.rb
+++ spec/defines/mcollective__user_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe 'mcollective::user' do
   let(:title) { 'nagios' }
-  let(:default_params) { { :securityprovider => 'ssl', :middleware_ssl => true, :connector => 'activemq', :middleware_hosts => ['localhost'] } }
+  let(:default_params) { { :connector => 'activemq', :middleware_hosts => ['localhost'] } }
   let(:params) { default_params }
   it { should contain_file('/home/nagios/.mcollective.d') }
   it { should contain_file('mcollective::user nagios').with_path('/home/nagios/.mcollective') }
 
   describe '#securityprovider' do
-    context 'default (ssl)' do
+    context 'ssl' do
+      let(:params) { default_params.merge({ :securityprovider => 'ssl' }) }
       it { should contain_file('/home/nagios/.mcollective.d/credentials/certs/nagios.pem') }
       it { should contain_file('/home/nagios/.mcollective.d/credentials/private_keys/nagios.pem') }
       it { should contain_mcollective__user__setting('nagios:plugin.ssl_client_public') }
@@ -17,7 +18,7 @@ describe 'mcollective::user' do
       it { should contain_mcollective__user__setting('nagios:plugin.ssl_server_public') }
     end
 
-    context 'specified (pies)' do
+    context 'pies' do
       let(:params) { default_params.merge({ :securityprovider => 'pies' }) }
       it { should_not contain_file('/home/nagios/.mcollective.d/credentials/certs/nagios.pem') }
       it { should_not contain_mcollective__user__setting('nagios:plugin.ssl_client_public') }
@@ -27,13 +28,14 @@ describe 'mcollective::user' do
   end
 
   describe '#middleware_ssl' do
-    context 'default (true)' do
+    context 'true' do
+      let(:params) { default_params.merge({ :middleware_ssl => true }) }
       it { should contain_file('/home/nagios/.mcollective.d/credentials/private_keys/nagios.pem') }
       it { should contain_mcollective__user__setting('nagios plugin.activemq.pool.1.ssl.cert') }
       it { should contain_mcollective__user__setting('nagios plugin.activemq.pool.1.ssl.cert').with_value('/home/nagios/.mcollective.d/credentials/certs/nagios.pem') }
     end
 
-    context 'specified (false)' do
+    context 'false' do
       let(:params) { default_params.merge({ :middleware_ssl => false }) }
       it { should_not contain_mcollective__user__setting('nagios plugin.activemq.pool.1.ssl.cert') }
     end
